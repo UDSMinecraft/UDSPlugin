67507378845d40956fc2a035cc18d47e812d09c2
UndeadScythes <daveyognaut2@gmail.com>
2012-11-07 03:55:06 +0000

    Renamed LoadableLocation to Bearing

--- src/com/undeadscythes/udsplugin/Bearing.java
+++ src/com/undeadscythes/udsplugin/Bearing.java
@@ -6,7 +6,7 @@ import org.bukkit.*;
  * A location that can be constructed by parsing a string.
  * @author UndeadScythes
  */
-public class LoadableLocation extends Location {
+public class Bearing extends Location {
     public static enum Direction {
         NORTH(-22.5, 22.5, true),
         NORTH_EAST(22.5, 67.5, false),
@@ -33,7 +33,7 @@ public class LoadableLocation extends Location {
             return (float)((this.max + this.min) / 2 + 180) % 360;
         }
 
-        public static Direction get(String string) {
+        public static Direction getByName(String string) {
             for(Direction test : values()) {
                 if(test.name().equals(string.toUpperCase())) {
                     return test;
@@ -64,16 +64,16 @@ public class LoadableLocation extends Location {
     }
 
     /**
-     * Initialises a brand new location.
+     * Initialises a brand new bearing.
      * @param string The location in the format "world,x,y,z,pitch,yaw".
      */
-    public LoadableLocation(String string) {
+    public Bearing(String string) {
         super(Bukkit.getWorld(string.split(",")[0]), Double.parseDouble(string.split(",")[1]), Double.parseDouble(string.split(",")[2]), Double.parseDouble(string.split(",")[3]));
         setPitch(Float.parseFloat(string.split(",")[4]));
         setPitch(Float.parseFloat(string.split(",")[5]));
     }
 
-    public LoadableLocation(Location location) {
+    public Bearing(Location location) {
         super(location.getWorld(), location.getX(), location.getY(), location.getZ(), location.getPitch(), location.getYaw());
     }
 
