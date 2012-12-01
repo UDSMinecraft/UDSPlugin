c3477f2a3f2461d33ceb2fb01209827bde2789ad
UndeadScythes <daveyognaut2@gmail.com>
2012-12-01 12:04:44 +0000

    Changed random variable type - Fixes #2

--- src/com/undeadscythes/udsplugin/eventhandlers/EntityDeath.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/EntityDeath.java
@@ -34,13 +34,9 @@ public class EntityDeath extends ListenerWrapper implements Listener {
     private void payReward(final SaveablePlayer player, final Entity victim) {
         if(player.getGameMode() == GameMode.SURVIVAL && !player.hasGodMode()) {
             final Random generator = new Random();
-            final int reward = Config.MOB_REWARDS.get(victim.getClass().getName().substring(35).toLowerCase()) * generator.nextInt();
+            final int reward = (int)(Config.MOB_REWARDS.get(victim.getClass().getName().substring(35).toLowerCase()) * generator.nextDouble());
             player.credit(reward);
-            if(reward == 1) {
-                player.sendMessage(Color.MESSAGE + "You picked up " + reward + " " + Config.CURRENCY + ".");
-            } else if(reward > 0) {
-                player.sendMessage(Color.MESSAGE + "You picked up " + reward + " " + Config.CURRENCIES + ".");
-            }
+            player.sendMessage(Color.MESSAGE + "You picked up " + reward + " " + (reward == 1 ? Config.CURRENCY : Config.CURRENCIES) + ".");
         }
     }
 }
