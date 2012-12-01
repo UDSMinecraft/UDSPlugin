efa210551af64a26778f3ca2c1a2c83d2eb59ee9
UndeadScythes <daveyognaut2@gmail.com>
2012-12-01 12:06:28 +0000

    Changed how null item affects flow - Fixes #10

--- src/com/undeadscythes/udsplugin/commands/ICmd.java
+++ src/com/undeadscythes/udsplugin/commands/ICmd.java
@@ -22,23 +22,23 @@ public class ICmd extends PlayerCommandExecutor {
                 } else if((amount = parseInt(args[1])) != -1) {
                     item.setAmount(amount);
                 }
-            }
-            if(player.hasPermission(Perm.I_ADMIN)) {
-                player.getInventory().addItem(item);
-            } else if(player.getVIPSpawns() > 0) {
-                if(Config.WHITELIST.contains(item.getType())) {
-                    if(item.getAmount() > player.getVIPSpawns()) {
-                        item.setAmount(player.getVIPSpawns());
-                    }
-                    if(player.useVIPSpawns(item.getAmount()) == 0) {
-                        player.sendMessage(Color.MESSAGE + "You have just used up your last spawns for today.");
-                    }
+                if(player.hasPermission(Perm.I_ADMIN)) {
                     player.getInventory().addItem(item);
+                } else if(player.getVIPSpawns() > 0) {
+                    if(Config.WHITELIST.contains(item.getType())) {
+                        if(item.getAmount() > player.getVIPSpawns()) {
+                            item.setAmount(player.getVIPSpawns());
+                        }
+                        if(player.useVIPSpawns(item.getAmount()) == 0) {
+                            player.sendMessage(Color.MESSAGE + "You have just used up your last spawns for today.");
+                        }
+                        player.getInventory().addItem(item);
+                    } else {
+                        player.sendMessage(Color.ERROR + "Sorry, " + item.getType().name().toLowerCase().replace("_", " ") + " is not a whitelisted item.");
+                    }
                 } else {
-                    player.sendMessage(Color.ERROR + "Sorry, " + item.getType().name().toLowerCase().replace("_", " ") + " is not a whitelisted item.");
+                    player.sendMessage(Color.ERROR + "You are out of spawns for today.");
                 }
-            } else {
-                player.sendMessage(Color.ERROR + "You are out of spawns for today.");
             }
         }
     }
