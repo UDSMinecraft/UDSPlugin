27bbed822f63e648bacda8ebca549698d926d062
UndeadScythes <daveyognaut2@gmail.com>
2012-12-02 00:55:29 +0000

    Added handler for double chests - Fixes #30

--- src/com/undeadscythes/udsplugin/eventhandlers/InventoryOpen.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/InventoryOpen.java
@@ -22,7 +22,14 @@ public class InventoryOpen extends ListenerWrapper implements Listener {
         if(event.getInventory().getHolder() == null) {
             return; // Anvil has no inventory holder.
         }
-        BlockState block = (BlockState)inventoryHolder;
+        BlockState block;
+        BlockState block2 = null;
+        if(inventoryHolder instanceof DoubleChest) {
+            block = (BlockState)((DoubleChest)inventoryHolder).getLeftSide();
+            block2 = (BlockState)((DoubleChest)inventoryHolder).getRightSide();
+        } else {
+            block = (BlockState)inventoryHolder;
+        }
         Location location = null;
         if(block.getType().equals(Material.ENDER_CHEST)) {
             event.setCancelled(true);
@@ -47,12 +54,48 @@ public class InventoryOpen extends ListenerWrapper implements Listener {
             player.sendMessage(Color.ERROR + "You do not have access to this block.");
             return;
         }
+        boolean checkDouble = true;
         if(block.getType().equals(Material.CHEST)) {
             location = block.getLocation();
             if(block.getBlock().getRelative(BlockFace.UP).getType().equals(Material.WALL_SIGN)) {
                 InventoryView inventoryView = event.getView();
                 Inventory shop = inventoryView.getTopInventory();
                 Sign sign = (Sign)block.getBlock().getRelative(BlockFace.UP).getState();
+                if(isShopSign(sign.getLines())) {
+                    checkDouble = false;
+                    String playerName = sign.getLine(0).replace(Color.SIGN.toString(), "");
+                    ItemStack item = findItem(sign.getLine(2));
+                    item.setAmount(64);
+                    if("Server".equals(playerName)) {
+                        for(int i = 0; i < 27; i++) {
+                            shop.setItem(i, item);
+                        }
+                    }
+                } else if(!player.canBuildHere(location)) {
+                    checkDouble = false;
+                    if(player.hasPermission(Perm.BYPASS)) {
+                        player.sendMessage(Color.MESSAGE + "Protection bypassed.");
+                        return;
+                    }
+                    event.setCancelled(true);
+                    player.sendMessage(Color.ERROR + "You do not have access to this block.");
+                }
+            } else if(!player.canBuildHere(location)) {
+                checkDouble = false;
+                if(player.hasPermission(Perm.BYPASS)) {
+                    player.sendMessage(Color.MESSAGE + "Protection bypassed.");
+                    return;
+                }
+                event.setCancelled(true);
+                player.sendMessage(Color.ERROR + "You do not have access to this block.");
+            }
+        }
+        if(checkDouble == true && block2 != null && block2.getType().equals(Material.CHEST)) {
+            location = block2.getLocation();
+            if(block2.getBlock().getRelative(BlockFace.UP).getType().equals(Material.WALL_SIGN)) {
+                InventoryView inventoryView = event.getView();
+                Inventory shop = inventoryView.getTopInventory();
+                Sign sign = (Sign)block2.getBlock().getRelative(BlockFace.UP).getState();
                 if(isShopSign(sign.getLines())) {
                     String playerName = sign.getLine(0).replace(Color.SIGN.toString(), "");
                     ItemStack item = findItem(sign.getLine(2));
