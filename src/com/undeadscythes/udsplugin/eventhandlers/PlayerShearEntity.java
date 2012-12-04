e2c7cb04c24bc68c174aa3f758b5b0bb2e5aece9
UndeadScythes <daveyognaut2@gmail.com>
2012-12-04 22:36:18 +0000

    Implemented shearing check

--- src/com/undeadscythes/udsplugin/eventhandlers/PlayerShearEntity.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/PlayerShearEntity.java
@@ -11,6 +11,10 @@ import org.bukkit.event.player.*;
 public class PlayerShearEntity extends ListenerWrapper implements Listener {
     @EventHandler
     public void onEvent(final PlayerShearEntityEvent event) {
-
+        final SaveablePlayer player = UDSPlugin.getOnlinePlayers().get(event.getPlayer().getName());
+        if(!player.canBuildHere(event.getEntity().getLocation())) {
+            event.setCancelled(true);
+            player.sendMessage(Color.ERROR + "You cannot shear this animal.");
+        }
     }
 }
