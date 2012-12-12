158536df3a7e2df52e3b0285179a56b5cf7d462c
UndeadScythes <daveyognaut2@gmail.com>
2012-12-12 13:41:24 +0000

    Removed implicit call to Clan.toString()

--- src/com/undeadscythes/udsplugin/commands/StatsCmd.java
+++ src/com/undeadscythes/udsplugin/commands/StatsCmd.java
@@ -14,7 +14,7 @@ public class StatsCmd extends AbstractPlayerCommand {
             player.sendMessage(Color.MESSAGE + target.getNick() + "'s stats:");
             player.sendMessage(Color.TEXT + "Minecraft name: " + target.getName());
             player.sendMessage(Color.TEXT + "Rank: " + target.getRank().getColor() + target.getRank().toString());
-            player.sendMessage(Color.TEXT + "Clan: " + (target.isInClan() ? target.getClan() : "not in clan"));
+            player.sendMessage(Color.TEXT + "Clan: " + (target.isInClan() ? target.getClan().getName() : "not in clan"));
             player.sendMessage(Color.TEXT + "Current bounty: " + target.getBounty());
             player.sendMessage(Color.TEXT + "Current level: " + target.getLevel());
             player.sendMessage(Color.TEXT + "Time logged: " + player.getTimeLogged());
