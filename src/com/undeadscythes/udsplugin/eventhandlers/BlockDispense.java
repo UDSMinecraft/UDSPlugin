b5522d03ef851d2a2d011f3e540ff25ac606c32f
UndeadScythes <daveyognaut2@gmail.com>
2012-12-15 22:58:14 +0000

    Added dispenser event handler

---
+++ src/com/undeadscythes/udsplugin/eventhandlers/BlockDispense.java
@@ -0,0 +1,16 @@
+package com.undeadscythes.udsplugin.eventhandlers;
+
+import com.undeadscythes.udsplugin.*;
+import org.bukkit.event.*;
+import org.bukkit.event.block.*;
+
+/**
+ * A block dispenses an item.
+ * @author UndeadScythes
+ */
+public class BlockDispense extends ListenerWrapper implements Listener {
+    @EventHandler
+    public void onEvent(final BlockDispenseEvent event) {
+
+    }
+}
