4790b65f3f6bbacd2b178aaf6aea4d63a70e695a
UndeadScythes <daveyognaut2@gmail.com>
2012-12-25 00:07:56 +0000

    Fixed entity interact checks - Fixes #113

--- src/com/undeadscythes/udsplugin/eventhandlers/PlayerInteractEntity.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/PlayerInteractEntity.java
@@ -11,22 +11,16 @@ import org.bukkit.event.player.*;
  */
 public class PlayerInteractEntity extends ListenerWrapper implements Listener {
     @EventHandler
-    public void onEvent(final PlayerInteractEntityEvent event) {
+    public final void onEvent(final PlayerInteractEntityEvent event) {
         if(event.getRightClicked() instanceof Tameable) {
-            final Tameable pet = (Tameable) event.getRightClicked();
+            final Tameable pet = (Tameable)event.getRightClicked();
             if(pet.isTamed()) {
-                String ownerName = "";
-                if(pet instanceof Wolf) {
-                    ownerName = ((Wolf)pet).getOwner().getName();
-                }
-                if(pet instanceof Ocelot) {
-                    ownerName = ((Ocelot)pet).getOwner().getName();
-                }
-                if(ownerName.equals(event.getPlayer().getName())) {
+                final String ownerName = pet.getOwner().getName();
+                if(ownerName.equals(event.getPlayer().getName()) && event.getPlayer().isSneaking()) {
                     UDSPlugin.getPlayers().get(event.getPlayer().getName()).selectPet(event.getRightClicked().getUniqueId());
                     event.getPlayer().sendMessage(Color.MESSAGE + "Pet selected.");
                     event.setCancelled(true);
-                } else if(event.getPlayer().isSneaking()){
+                } else {
                     event.getPlayer().sendMessage(Color.MESSAGE + "This animal belongs to " + UDSPlugin.getPlayers().get(ownerName).getNick());
                 }
             }
