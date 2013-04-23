a9da1982a057ea2d62c18fcd1db0e3e0cbfd5468
UndeadScythes <daveyognaut2@gmail.com>
2013-04-23 22:16:35 +0100

    Added EventHandler tag - Fixes #153

--- src/com/undeadscythes/udsplugin/eventhandlers/PlayerLogin.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/PlayerLogin.java
@@ -1,6 +1,7 @@
 package com.undeadscythes.udsplugin.eventhandlers;
 
 import org.bukkit.*;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
@@ -9,8 +10,9 @@ import org.bukkit.event.player.PlayerLoginEvent;
  * @author Dave
  */
 public class PlayerLogin implements Listener {
+    @EventHandler
     public void onEvent(final PlayerLoginEvent event) {
-        if(Bukkit.getServer().hasWhitelist() && !event.getPlayer().isWhitelisted()) {
+        if(Bukkit.hasWhitelist() && !event.getPlayer().isWhitelisted()) {
             event.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, "The server is currently whitelisted while we make major improvements. Thank you for your patience.");
         }
     }
