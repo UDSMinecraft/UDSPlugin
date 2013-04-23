c7d60123cee9e2dde7594269c1b31c778e998eca
UndeadScythes <daveyognaut2@gmail.com>
2013-04-23 21:42:54 +0100

    Added TNT carts to explosion list

--- src/com/undeadscythes/udsplugin/eventhandlers/EntityExplode.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/EntityExplode.java
@@ -14,7 +14,7 @@ public class EntityExplode extends ListenerWrapper implements Listener {
     public final void onEvent(final EntityExplodeEvent event) {
         if(event.getEntityType().equals(EntityType.CREEPER) && UDSPlugin.getConfigBool(ConfigRef.BLOCK_CREEPERS)) {
             event.blockList().clear();
-        } else if(event.getEntityType().equals(EntityType.PRIMED_TNT) && UDSPlugin.getConfigBool(ConfigRef.BLOCK_TNT)) {
+        } else if((event.getEntityType().equals(EntityType.PRIMED_TNT) || event.getEntityType().equals(EntityType.MINECART_TNT)) && UDSPlugin.getConfigBool(ConfigRef.BLOCK_TNT)) {
             event.blockList().clear();
         } else if((event.getEntityType().equals(EntityType.WITHER_SKULL) || event.getEntityType().equals(EntityType.WITHER)) && UDSPlugin.getConfigBool(ConfigRef.BLOCK_WITHER)) {
             event.blockList().clear();
