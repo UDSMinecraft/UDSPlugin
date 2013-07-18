29f8517c6c782c1af9c7648bf2b83ffb24245794
UndeadScythes <daveyognaut2@gmail.com>
2013-07-18 15:41:21 +0100

    Changed handling of entities - Closes # 170

--- src/com/undeadscythes/udsplugin/eventhandlers/PlayerInteractEntity.java
+++ src/com/undeadscythes/udsplugin/eventhandlers/PlayerInteractEntity.java
@@ -12,21 +12,23 @@ import org.bukkit.event.player.*;
 public class PlayerInteractEntity extends ListenerWrapper implements Listener {
     @EventHandler
     public final void onEvent(final PlayerInteractEntityEvent event) {
-        if(event.getRightClicked() instanceof Tameable) {
-            final Tameable pet = (Tameable)event.getRightClicked();
+        final Entity entity = event.getRightClicked();
+        if(entity instanceof Tameable) {
+            final Tameable pet = (Tameable)entity;
             if(pet.isTamed()) {
                 final String ownerName = pet.getOwner().getName();
                 if(ownerName.equals(event.getPlayer().getName()) && event.getPlayer().isSneaking()) {
-                    UDSPlugin.getPlayers().get(event.getPlayer().getName()).selectPet(event.getRightClicked().getUniqueId());
+                    UDSPlugin.getPlayers().get(event.getPlayer().getName()).selectPet(entity.getUniqueId());
                     event.getPlayer().sendMessage(Color.MESSAGE + "Pet selected.");
                     event.setCancelled(true);
                 } else {
                     event.getPlayer().sendMessage(Color.MESSAGE + "This animal belongs to " + UDSPlugin.getPlayers().get(ownerName).getNick());
+                    event.setCancelled(true);
                 }
             }
-        } else if(event.getRightClicked() instanceof ItemFrame) {
+        } else if(entity instanceof ItemFrame) {
             final SaveablePlayer player = UDSPlugin.getPlayers().get(event.getPlayer().getName());
-            if(!(player.canBuildHere(event.getRightClicked().getLocation()))) {
+            if(!(player.canBuildHere(entity.getLocation()))) {
                 event.setCancelled(true);
                 player.sendMessage(Message.CANT_BUILD_HERE);
             }
