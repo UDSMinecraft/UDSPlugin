67f4da9cc231ffab1278aa1a3e3ad15958c688d2
UndeadScythes <daveyognaut2@gmail.com>
2013-08-02 19:53:35 +0100

    Added base ConfigCmd

---
+++ src/com/undeadscythes/udsplugin/commands/ConfigCmd.java
@@ -0,0 +1,15 @@
+package com.undeadscythes.udsplugin.commands;
+
+/**
+ * Allows editing of the configuration yaml from in-game or the console.
+ * 
+ * @author UndeadScythes
+ */
+public class ConfigCmd extends CommandValidator {
+    @Override
+    public void playerExecute() {
+        if(minArgsHelp(2)) {
+            
+        }
+    }
+}
