92a7b91567eca79c0f888eacff1319a24ea9861f
UndeadScythes <daveyognaut2@gmail.com>
2013-08-12 14:48:38 +0100

    Added coordinates to where command - Closes #264

--- src/com/undeadscythes/udsplugin/commands/WhereCmd.java
+++ src/com/undeadscythes/udsplugin/commands/WhereCmd.java
@@ -4,24 +4,24 @@ import org.bukkit.*;
 
 /**
  * Gives a description of the players surroundings.
- * 
+ *
  * @author UndeadScythes
  */
 public class WhereCmd extends CommandHandler {
     @Override
     public final void playerExecute() {
-        final Location playerLocation = player().getLocation();
-        final Location spawnLocation = player().getWorld().getSpawnLocation();
-        final int distance = (int)playerLocation.distance(spawnLocation);
-        String message = "You are " + distance + " blocks from spawn in world " + playerLocation.getWorld().getName() + ",";
-        if(player().getLocation().getBlockY() < 64) {
-            message = message.concat(" " + (64 - player().getLocation().getBlockY()) + " blocks below sea level");
-        } else if(player().getLocation().getBlockY() > 64) {
-            message = message.concat(" " + (player().getLocation().getBlockY() - 64) + " blocks above sea level");
+        final Location location = player().getLocation();
+        final Location spawn = player().getWorld().getSpawnLocation();
+        String message = "You are " + (int)location.distance(spawn) + " blocks from spawn in world " + location.getWorld().getName() + ",";
+        if(location.getBlockY() < 64) {
+            message = message.concat(" " + (64 - location.getBlockY()) + " blocks below sea level");
+        } else if(location.getBlockY() > 64) {
+            message = message.concat(" " + (location.getBlockY() - 64) + " blocks above sea level");
         } else {
             message = message.concat(" at sea level");
         }
-        message = message.concat(" in a " + player().getLocation().getBlock().getBiome().toString().toLowerCase().replace("_", " ") + " biome");
+        message = message.concat(" in a " + location.getBlock().getBiome().toString().toLowerCase().replace("_", " ") + " biome.");
         player().sendNormal(message);
+        player().sendNormal("Your coordinates are x:" + location.getBlockX() + " y:" + location.getBlockY() + " z:" + location.getBlockZ() + ".");
     }
 }
