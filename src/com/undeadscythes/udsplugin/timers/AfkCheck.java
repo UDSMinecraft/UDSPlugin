b08511d1ac3c756d6378d0ef4c39b2b7bf46e744
UndeadScythes <daveyognaut2@gmail.com>
2013-08-13 11:47:33 +0100

    Fixed some bugs
    Also removed whitespace.

--- src/com/undeadscythes/udsplugin/timers/AfkCheck.java
+++ src/com/undeadscythes/udsplugin/timers/AfkCheck.java
@@ -2,17 +2,20 @@ package com.undeadscythes.udsplugin.timers;
 
 import com.undeadscythes.udsplugin.*;
 import com.undeadscythes.udsplugin.utilities.*;
-import org.bukkit.util.*;
+import java.util.*;
+import org.bukkit.util.Vector;
 
 /**
  * Scheduled task to check if a player is AFK.
- * 
+ *
  * @author UndeadScythes
  */
 public class AfkCheck implements Runnable {
     @Override
     public final void run() {
-        for(final SaveablePlayer player : PlayerUtils.getOnlinePlayers()) {
+        Iterator<SaveablePlayer> i = PlayerUtils.getOnlinePlayers().iterator();
+        while(i.hasNext()) {
+            SaveablePlayer player = i.next();
             final Vector vector = VectorUtils.getFlooredVector(player.getLocation().toVector());
             if(vector.isInSphere(player.getLastVector(), 1)) {
                 player.kickPlayer("You have been kicked for idling.");
--- src/com/undeadscythes/udsplugin/utilities/PlayerUtils.java
+++ src/com/undeadscythes/udsplugin/utilities/PlayerUtils.java
@@ -10,7 +10,7 @@ import org.bukkit.inventory.*;
 
 /**
  * Utility class for handling manipulations with {@link SaveablePlayer} objects.
- * 
+ *
  * @author UndeadScythes
  */
 public class PlayerUtils {
@@ -24,27 +24,27 @@ public class PlayerUtils {
     public static Collection<SaveablePlayer> getPlayers() {
         return PLAYERS.values();
     }
-    
+
     public static SaveablePlayer getPlayer(final String name) {
         return PLAYERS.get(name);
     }
-    
+
     public static List<SaveablePlayer> getSortedPlayers(final Comparator<SaveablePlayer> comparator) {
         return PLAYERS.getSortedValues(comparator);
     }
-    
+
     public static SaveablePlayer matchPlayer(final String partial) {
         return PLAYERS.matchKey(partial);
     }
-    
+
     public static void addPlayer(final SaveablePlayer player) {
         PLAYERS.put(player.getName(), player);
     }
-    
+
     public static boolean playerExists(final String name) {
         return PLAYERS.containsKey(name);
     }
-    
+
     public static int numPlayers() {
         return PLAYERS.size();
     }
@@ -56,11 +56,11 @@ public class PlayerUtils {
     public static Collection<SaveablePlayer> getHiddenPlayers() {
         return HIDDEN_PLAYERS.values();
     }
-    
+
     public static void addHiddenPlayer(final SaveablePlayer player) {
         HIDDEN_PLAYERS.put(player.getName(), player);
     }
-    
+
     public static void removeHiddenPlayer(final String name) {
         HIDDEN_PLAYERS.remove(name);
     }
@@ -68,34 +68,34 @@ public class PlayerUtils {
     public static Collection<SaveablePlayer> getOnlinePlayers() {
         return ONLINE_PLAYERS.values();
     }
-    
+
     public static SaveablePlayer getOnlinePlayer(final String name) {
         return ONLINE_PLAYERS.get(name);
     }
-    
+
     public static SaveablePlayer getOnlinePlayer(final Player player) {
         return ONLINE_PLAYERS.get(player.getName());
     }
-    
+
     public static SaveablePlayer matchOnlinePlayer(final String partial) {
         return ONLINE_PLAYERS.matchKey(partial);
     }
-    
+
     public static void addOnlinePlayer(final SaveablePlayer player) {
         ONLINE_PLAYERS.put(player.getName(), player);
     }
-    
+
     public static SaveablePlayer removeOnlinePlayer(final String name) {
         return ONLINE_PLAYERS.remove(name);
     }
-    
+
     public static void savePlayers(final File parent) throws IOException {
         PLAYERS.save(parent + File.separator + FILENAME);
         for(final YamlConfig config : INVENTORIES.values()) {
             config.save();
         }
     }
-    
+
     public static void loadPlayers(final File parent) throws IOException {
         try {
             final BufferedReader file = new BufferedReader(new FileReader(parent + File.separator + FILENAME));
@@ -114,38 +114,38 @@ public class PlayerUtils {
             INVENTORIES.get(world).load();
         }
     }
-    
+
     public static void saveInventory(final SaveablePlayer player, final World world) {
         final FileConfiguration config = INVENTORIES.get(world).getConfig();
         config.set(player.getName() + ".inventory", player.getInventory().getContents());
         config.set(player.getName() + ".armor", player.getInventory().getArmorContents());
     }
-    
+
     public static void loadInventory(final SaveablePlayer player, final World world) {
         final FileConfiguration config = INVENTORIES.get(world).getConfig();
         if(config.contains(player.getName() + ".inventory") && config.contains(player.getName() + ".armor")) {
-            player.getInventory().setContents((ItemStack[])config.get(player.getName() + ".inventory"));
-            player.getInventory().setArmorContents((ItemStack[])config.get(player.getName() + ".armor"));
+            player.getInventory().setContents(config.getList(player.getName() + ".inventory").toArray(new ItemStack[36]));
+            player.getInventory().setArmorContents(config.getList(player.getName() + ".armor").toArray(new ItemStack[4]));
         } else {
             player.getInventory().clear(-1, -1);
         }
     }
-    
+
     public static void saveInventory(final SaveablePlayer player) {
         saveInventory(player, player.getWorld());
     }
-    
+
     public static void loadInventory(final SaveablePlayer player) {
         loadInventory(player, player.getWorld());
     }
-    
+
     public static int numActivePlayers() {
         int count = 0;
         for(final SaveablePlayer player : PLAYERS.values()) {
             count += player.isActive() ? 1 : 0;
         }
         return count;
-    } 
-    
+    }
+
     private PlayerUtils() {}
 }
