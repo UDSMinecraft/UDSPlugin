66aab6c128809d45a95c999b93c58be0edc138aa
UndeadScythes <daveyognaut2@gmail.com>
2013-08-13 15:30:06 +0100

    Changed how VIP timeouts occur
    Also removed whitespace.

--- src/com/undeadscythes/udsplugin/timers/PlayerChecks.java
+++ src/com/undeadscythes/udsplugin/timers/PlayerChecks.java
@@ -5,7 +5,7 @@ import com.undeadscythes.udsplugin.utilities.*;
 
 /**
  * Scheduled task to perform various player checks.
- * 
+ *
  * @author UndeadScythes
  */
 public class PlayerChecks implements Runnable {
@@ -21,13 +21,13 @@ public class PlayerChecks implements Runnable {
             pvpStatus(player);
         }
     }
-    
+
     private void pvpStatus(final SaveablePlayer player) {
         if(player.getKills() > 0 && player.getPvpTime() + TimeUtils.HOUR < System.currentTimeMillis()) {
                 player.removeKill();
         }
     }
-    
+
     private void checkLocation(final SaveablePlayer player) {
         final int distanceSq = Math.abs((int)Math.pow(player.getLocation().getBlockX(), 2) + (int)Math.pow(player.getLocation().getBlockZ(), 2));
         if(distanceSq - Config.WORLD_BORDER_SQRD > 100) {
@@ -36,22 +36,22 @@ public class PlayerChecks implements Runnable {
             player.sendNormal("You have reached the edge of the currently explorable world.");
         }
     }
-    
+
     private void godMode(final SaveablePlayer player) {
         if(player.hasGodMode()) {
             player.setFoodLevel(20);
         }
     }
-    
+
     private void jailStatus(final SaveablePlayer player) {
         if(player.isJailed() && player.getJailTime() + player.getJailSentence() < System.currentTimeMillis()) {
             player.release();
             player.sendNormal("You have served your time.");
         }
     }
-    
+
     private void vipStatus(final SaveablePlayer player) {
-        if(player.hasPerm(Perm.VIP_RANK) && player.getVIPTime() + Config.VIP_TIME < System.currentTimeMillis()) {
+        if(player.hasPerm(Perm.VIP_RANK) && !player.isVipForLife() && player.getVIPTime() + Config.VIP_TIME < System.currentTimeMillis()) {
             player.setVIPTime(0);
             player.setRank(PlayerRank.MEMBER);
             player.sendNormal("Your time as a VIP has come to an end.");
